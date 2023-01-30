ac34f7a8e4efa43f49032018c8d803e9da114e35
Khuong Tran <dbcgdhh@gmail.com>
2023-01-30 21:05:21 +0700

    Add on click Add button

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,9 +1,10 @@
 <template>
   <footer>
-    <button
-      class="fixed bottom-6 right-6 w-10 h-10 rounded-full bg-black text-2xl text-white"
+    <RouterLink
+      class="fixed bottom-6 right-6 w-10 h-10 rounded-full bg-black text-2xl text-white text-center"
+      to="add"
     >
-      <span class="relative -top-px">+</span>
-    </button>
+      <span class="relative top-px">+</span>
+    </RouterLink>
   </footer>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: () => import("@/views/Home.vue") },
     { path: "/add", name: "Add", component: () => import("@/views/Add.vue") },
