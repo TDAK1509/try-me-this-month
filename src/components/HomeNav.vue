543330d45d070ec36f13bfedbca1c192f0bdadf2
Khuong Tran <dbcgdhh@gmail.com>
2023-01-31 14:32:49 +0700

    On click change current tab

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -4,7 +4,9 @@
       <li class="flex-1" v-for="category in categories" :key="category">
         <button
           class="w-full py-3 bg-gray-200"
-          :class="{ 'bg-gray-900 text-white': category === '2tr5' }"
+          :class="{ 'bg-gray-900 text-white': category === currentCategory }"
+          :disabled="category === currentCategory"
+          @click="currentCategory = category"
         >
           {{ category }}
         </button>
@@ -14,5 +16,8 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
+
 const categories = ["This month", "2tr5", "3tr5"];
+const currentCategory = ref("2tr5");
 </script>
