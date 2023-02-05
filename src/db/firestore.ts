8122e7e0e267424abf428e3f7579788dc037d199
Khuong Tran <dbcgdhh@gmail.com>
2023-02-05 14:31:03 +0700

    Remove unnecessary firebase config

--- src/db/firestore.ts
+++ src/db/firestore.ts
@@ -16,9 +16,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyAmftY35GwnoteVn_ixIRwbCWSrAJiSr3U",
   authDomain: "try-me-this-month.firebaseapp.com",
   projectId: "try-me-this-month",
-  // storageBucket: "try-me-this-month.appspot.com",
-  // messagingSenderId: "191615141577",
-  appId: "1:191615141577:web:8948d78a7f9362766a2f18",
 };
 
 const app = initializeApp(firebaseConfig);
