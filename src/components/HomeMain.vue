874aff1973ed27f62358fcf1b6c9b8531f703bfb
Khuong Tran <dbcgdhh@gmail.com>
2023-02-05 10:57:54 +0700

    Add animation remove list

--- src/components/HomeMain.vue
+++ src/components/HomeMain.vue
@@ -3,17 +3,19 @@
     <template v-for="(links, subName) in selectedPriceLinks">
       <h4 class="font-bold mt-4 first:mt-0">{{ subName }}</h4>
       <ul class="list-disc text-blue-500">
-        <li v-for="link in links" class="ml-4 my-2">
-          <a href="link" target="_blank" class="underline">{{ link }}</a>
-          <button
-            class="ml-4 rotate-45 text-gray-700 text-2xl relative top-0.5 cursor-pointer"
-            :disabled="isDeleting"
-            @click="removeLink(subName, link)"
-          >
-            <Loader v-if="isDeleting && link === linkBeingDeleted" />
-            <span v-else>+</span>
-          </button>
-        </li>
+        <TransitionGroup name="list">
+          <li v-for="link in links" :key="link" class="ml-4 my-2">
+            <a href="link" target="_blank" class="underline">{{ link }}</a>
+            <button
+              class="ml-4 rotate-45 text-gray-700 text-2xl relative top-0.5 cursor-pointer"
+              :disabled="isDeleting"
+              @click="removeLink(subName, link)"
+            >
+              <Loader v-if="isDeleting && link === linkBeingDeleted" />
+              <span v-else>+</span>
+            </button>
+          </li>
+        </TransitionGroup>
       </ul>
     </template>
   </main>
@@ -33,7 +35,7 @@ async function removeLink(subName: SubName, link: Link) {
   try {
     isDeleting.value = true;
     linkBeingDeleted.value = link;
-    // await remove(subName, selectedPrice.value, link);
+    await remove(subName, selectedPrice.value, link);
     removeLinkInUi(subName, link);
   } catch (e: unknown) {
     if (e instanceof Error) alert(e.message);
