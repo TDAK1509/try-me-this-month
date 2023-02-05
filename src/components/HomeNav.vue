7be2636486d55cff827adc840a132f39c7b22374
Khuong Tran <dbcgdhh@gmail.com>
2023-02-05 11:15:18 +0700

    Make nav scrollable

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -1,7 +1,11 @@
 <template>
   <nav class="nav">
-    <ul class="flex text-center">
-      <li class="flex-1" v-for="category in priceList" :key="category">
+    <ul class="flex text-center overflow-x-auto">
+      <li
+        class="basis-28 shrink-0"
+        v-for="category in priceList"
+        :key="category"
+      >
         <button
           class="w-full py-3 bg-gray-200"
           :class="{ 'bg-gray-900 text-white': category === selectedPrice }"
