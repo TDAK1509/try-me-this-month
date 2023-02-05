0bb0bd6409209eef9c4cfae92b2aa18e6dda510e
Khuong Tran <dbcgdhh@gmail.com>
2023-02-05 10:21:21 +0700

    Add loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loader w-5 h-5 border-2"></div>
+</template>
+
+<style coped>
+.loader {
+  display: inline-block;
+  border-color: transparent;
+  border-top-color: #fff;
+  border-radius: 50%;
+  animation: spin 0.7s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
--- src/views/Add.vue
+++ src/views/Add.vue
@@ -19,8 +19,13 @@
       <button
         class="bg-black text-white px-6 py-2 rounded mt-6 w-full cursor-pointer"
       >
-        Add
+        <Loader v-if="loading" />
+        <span v-else>Add</span>
       </button>
+
+      <p v-if="shouldShowSuccessMessage" class="text-lime-500 text-xs mt-3">
+        Successfully added.
+      </p>
 
       <RouterLink class="block mt-4 italic text-base" type="button" to="/">
         &lt; Back
@@ -31,6 +36,7 @@
 
 <script lang="ts" setup>
 import { ref } from "vue";
+import Loader from "@/components/Loader.vue";
 import { useSub } from "@/store/sub";
 
 const { add } = useSub();
@@ -41,11 +47,19 @@ const sub = ref("");
 const price = ref("");
 const link = ref("");
 
+const loading = ref(false);
+const shouldShowSuccessMessage = ref(false);
+
 async function submit() {
   try {
+    loading.value = true;
+    shouldShowSuccessMessage.value = false;
     await add(sub.value, price.value, link.value);
+    shouldShowSuccessMessage.value = true;
   } catch (e) {
     console.error("Error adding document: ", e);
+  } finally {
+    loading.value = false;
   }
 }
 </script>
