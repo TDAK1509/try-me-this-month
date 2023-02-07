887d5f7dc5d1ae952862ff01d539c2f5b231e0c7
Khuong Tran <dbcgdhh@gmail.com>
2023-02-07 09:13:19 +0700

    Init heart button

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,10 +1,16 @@
 <template>
-  <footer>
+  <footer class="flex flex-col fixed bottom-6 right-6">
+    <HeartButton />
+
     <RouterLink
-      class="fixed bottom-6 right-6 w-10 h-10 rounded-full bg-black text-2xl text-white text-center"
+      class="w-10 h-10 rounded-full bg-black text-2xl text-white text-center"
       to="add"
     >
       <span class="relative top-px">+</span>
     </RouterLink>
   </footer>
 </template>
+
+<script setup lang="ts">
+import HeartButton from "./HeartButton.vue";
+</script>
