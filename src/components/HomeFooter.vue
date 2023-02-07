a1c60733ca648f3c5edf30a9d5dac244fea04e6b
Khuong Tran <dbcgdhh@gmail.com>
2023-02-07 09:22:41 +0700

    Create favorite button

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -1,6 +1,11 @@
 <template>
-  <footer class="flex flex-col fixed bottom-6 right-6">
-    <HeartButton />
+  <footer class="flex flex-col fixed bottom-12 right-6 gap-4">
+    <RouterLink
+      class="w-10 h-10 rounded-full bg-red-500 text-2xl flex justify-center center-center pt-2.5"
+      to="favorite"
+    >
+      <HeartButton width="20px" height="20px" color="white" />
+    </RouterLink>
 
     <RouterLink
       class="w-10 h-10 rounded-full bg-black text-2xl text-white text-center"
