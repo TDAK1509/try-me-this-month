ad8419df33a49681d39e8082b40f77617179710e
Khuong Tran <dbcgdhh@gmail.com>
2023-02-07 10:20:26 +0700

    Allow remove ravorite in favorite page

--- src/views/Favorite.vue
+++ src/views/Favorite.vue
@@ -23,9 +23,23 @@
       </button>
     </nav>
 
-    <ul class="ml-4 list-disc text-blue-500">
+    <ul class="ml-4 list-disc">
       <li v-for="link in favorites" :key="link" class="mb-2">
-        <a :href="link" target="_blank" class="underline">{{ link }}</a>
+        <a :href="link" target="_blank" class="underline text-blue-500">{{
+          link
+        }}</a>
+
+        <button
+          class="w-3 h-3 ml-4 cursor-pointer"
+          :disabled="isRemovingFavorite"
+          @click="onClickHeartButton(link)"
+        >
+          <Loader
+            v-if="isRemovingFavorite && link === linkBeingRemoved"
+            class="w-3 h-3"
+          />
+          <HeartButton v-else />
+        </button>
       </li>
     </ul>
   </div>
@@ -34,11 +48,14 @@
 <script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import Loader from "@/components/Loader.vue";
+import HeartButton from "@/components/HeartButton.vue";
 import { useSub } from "@/store/sub";
 
-const { favorites, fetchFavorites } = useSub();
+const { favorites, fetchFavorites, removeFavorite } = useSub();
 
 const loading = ref(true);
+const isRemovingFavorite = ref(false);
+const linkBeingRemoved = ref("");
 
 onMounted(async () => {
   await fetchFavorites();
@@ -52,5 +69,20 @@ function copy() {
 
 function copyToClipboard(text: string) {
   navigator.clipboard.writeText(text);
+}
+
+async function onClickHeartButton(link: string) {
+  try {
+    isRemovingFavorite.value = true;
+    linkBeingRemoved.value = link;
+    await removeFavorite(link);
+    await fetchFavorites();
+  } catch (e: unknown) {
+    if (e instanceof Error) alert(e.message);
+    alert(e);
+  } finally {
+    isRemovingFavorite.value = false;
+    linkBeingRemoved.value = "";
+  }
 }
 </script>
